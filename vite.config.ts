f097acd6fcc523a6b6a4cff3fa64a303a4ce4dcb
kenny <kennys_wang@163.com>
2022-09-08 22:38:27 +0800

    rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
-import { join } from 'path'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
